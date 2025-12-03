607c6fde318e37ecc2f8764dcb2b405bad2a8d53
GTK-kala <gtkkala1110@gmail.com>
2025-12-03 21:23:14 +0300

    Add initial project structure with database connection, routes, and views

---
+++ Config/DataBase.js
@@ -0,0 +1,16 @@
+import mysql2 from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
+
+const db = mysql2.createConnection({
+  host: process.env.Host,
+  user: process.env.User,
+  password: process.env.Password,
+  database: process.env.Database,
+});
+
+db.connect(() => {
+  console.log("Database connected");
+});
+
+export default db;
--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+import route from "./Routes/routes.js";
+import { fileURLToPath } from "url";
 import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+// Configure __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, ".env") });
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+
+app.set("view engine", "hbs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(route);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
