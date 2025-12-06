9b7ef9defff310abb860cd1a58879b99508f67df
GTK-kala <gtkkala1110@gmail.com>
2025-12-06 21:47:14 +0300

    Refactor token verification: replace middleware import and ensure proper token handling in user verification route

--- Server/Routes/routes.js
+++ Server/Routes/routes.js
@@ -3,14 +3,14 @@ import express from "express";
 import { LoginUser } from "../Controller/Login.js";
 import { SignUpUser } from "../Controller/Signup.js";
 import { LogoutUser } from "../Controller/Logout.js";
-import { verifyToken } from "../middleware/verifyToken .js";
 import { verifyUser } from "../Controller/VerifyUser.js";
+import { VerifyToken } from "../Users/verifyToken .js";
 
 const route = express.Router();
 
 route.post("/login", LoginUser);
 route.post("/signup", SignUpUser);
-route.get("/verify/user", verifyToken, verifyUser);
+route.get("/verify/user", VerifyToken, verifyUser);
 route.delete("/logout/:users_id", LogoutUser);
 
 export default route;
